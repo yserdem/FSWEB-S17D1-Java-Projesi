d0616a54a844dc96c972b1ccc9f20d5fe3f3b03b
Sinan <y.sinanerdem@gmail.com>
2023-08-21 15:50:40 +0300

    init

---
+++ src/main/java/com/workintech/FSWEBS17D1JavaProjesi/FswebS17D1JavaProjesiApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.FSWEBS17D1JavaProjesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FswebS17D1JavaProjesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FswebS17D1JavaProjesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/FSWEBS17D1JavaProjesi/FswebS17D1JavaProjesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.FSWEBS17D1JavaProjesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FswebS17D1JavaProjesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
